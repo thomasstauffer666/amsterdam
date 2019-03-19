a2349493850d0039b68308318dc45e8f3a210868
Thomas Stauffer <thomasstauffer@gmx.ch>
2019-03-19 10:43:26 +0100

    prototypes/js-two-client-server: worker cache issue

--- prototypes/js-two-client-server/server-worker.js
+++ prototypes/js-two-client-server/server-worker.js
@@ -1,12 +1,16 @@
 'use strict';
 
-importScripts('require.js');
-importScripts('config.js');
-importScripts('functions.js');
-importScripts('server.js');
+// Firefox/Chrome cache it only this script and do not even make a request to the server anymore, if the script is also added as a <script> it seems to work
 
-const server = require('./server.js');
+if (typeof window === 'undefined') {
+  importScripts('require.js');
+  importScripts('config.js');
+  importScripts('functions.js');
+  importScripts('server.js');
 
-self.onmessage = server.serverMessageFromWorker;
-server.serverWorkerOpen();
-server.worldStartup();
+  const server = require('./server.js');
+
+  self.onmessage = server.serverMessageFromWorker;
+  server.serverWorkerOpen();
+  server.worldStartup();
+}
